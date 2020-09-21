622a1225a03b72e7afbd65421c6c4b7dfc82d6f2
JKamue <mail@jkamue.de>
2020-09-21 21:58:21 +0200

    Added Course and Question dto

---
+++ 15PunkteQuiz/Data/CourseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15PunkteQuiz.Data
+{
+    class CourseDto
+    {
+        public string Id;
+        public string Name;
+
+        public CourseDto(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ 15PunkteQuiz/Data/QuestionDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15PunkteQuiz.Data
+{
+    class QuestionDto
+    {
+        public string Id;
+        public string Title;
+        public int Difficulty;
+        public List<string> WrongAnswers;
+        public string RightAnswer;
+
+        public QuestionDto(string id, string title, int difficulty, List<string> wrongAnswers, string rightAnswer)
+        {
+            Id = id;
+            Title = title;
+            Difficulty = difficulty;
+            WrongAnswers = wrongAnswers;
+            RightAnswer = rightAnswer;
+        }
+    }
+}
